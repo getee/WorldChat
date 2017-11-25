4715d092f28513b2978fc5c6536467afaf45c4ff
asusk <asusk@Mamoray-OE230HA>
2017-11-25 21:52:45 +0800

    添加了cotral

---
+++ WorldChat/src/com/getee/worldchat/control/DBOperation.java
@@ -0,0 +1,117 @@
+package com.getee.worldchat.control;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.util.Map;
+
+import com.getee.worldchat.model.User;
+
+public class DBOperation {
+    /**
+     * 对用户的基本信息进行 验证，保存，或修改
+     * @return
+     */
+    public static void main(String[] args) {
+        User admin=new User();
+    }
+    public static User login(String idNum,String password)//验证登录
+    {
+//        File file=new File("userinfo/"+idNum+".wc");
+//        if(!file.exists())return null;
+        User user=select(idNum);//查询出账户信息
+        if(user==null)return null;
+        if(user.getPassword().equals(password))return user;//返回登录的用户所有信息
+        return null;
+        
+    }
+    public static User select(String idNum)//查找账户。。。。添加好友
+    {
+        File file=new File("userinfo/"+idNum+".wc");
+        if(!file.exists())return null;
+        
+        ObjectInputStream obj = null;
+        try {
+            obj=new ObjectInputStream(new FileInputStream(file));
+            User user=(User)obj.readObject();//把查找到的账户返回
+            return user;//返回查找的用户所有信息
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally{
+            try {
+                if(obj!=null)
+                    obj.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+    public static User addFriend(User user,String addID,String groupName)//添加好友信息(自己类，添加的好友id,分组名)
+    {
+        Map<String,String> friend=user.getFriends();
+        friend.put(addID, groupName);
+        user.setFriends(friend);
+        return user;
+        
+    }
+
+    public static boolean add(User user)//添加注册信息=======在界面出进行new HashMap==
+    {
+        File file=new File("userinfo/"+user.getIdNum()+".wc");
+        if(file.exists()) return false;
+        else{
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return edit(user);
+    }
+    public static boolean edit(User user)//修改信息----需要在修改界面处判断修改后的id是否被注册(或阻止ID修改)----
+    {
+        File file=new File("userinfo/"+user.getIdNum()+".wc");
+        ObjectOutputStream obj = null;
+        try {
+            obj=new ObjectOutputStream(new FileOutputStream(file));
+            obj.writeObject(user);
+            obj.flush();
+            
+            return true;
+            
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally{
+            try {
+                if(obj!=null)
+                    obj.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        
+        return false;
+    }
+    //public static boolean delete(User user)//账号不删除
+    
+}
---
+++ WorldChat/src/com/getee/worldchat/model/User.java
@@ -0,0 +1,70 @@
+package com.getee.worldchat.model;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class User implements Serializable {
+    /**
+     * 存储用户的基本信息
+     */
+    private String idNum;//账号名
+    private String password;//密码
+    private String niname;//昵称
+    private String sex;//性别
+    private String speakword;//个性签名
+    private Map<String,String> friends;//列表:<好友账号,分组名>
+    
+    public User(String idNum, String password, String niname, String sex,
+            String speakword) {
+        this.idNum = idNum;
+        this.password = password;
+        this.niname = niname;
+        this.sex = sex;
+        this.speakword = speakword;
+        this.friends = new TreeMap<>();//好友不能直接构造
+    }
+    public User()
+    {
+        
+    }
+    public String getIdNum() {
+        return idNum;
+    }
+    public void setIdNum(String idNum) {
+        this.idNum = idNum;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getNiname() {
+        return niname;
+    }
+    public void setNiname(String niname) {
+        this.niname = niname;
+    }
+    public String getSex() {
+        return sex;
+    }
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+    public String getSpeakword() {
+        return speakword;
+    }
+    public void setSpeakword(String speakword) {
+        this.speakword = speakword;
+    }
+    public Map<String, String> getFriends() {
+        return friends;
+    }
+    public void setFriends(Map<String, String> friends) {
+        this.friends = friends;
+    }
+    
+
+}
